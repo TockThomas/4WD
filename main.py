c364f664ba2d09594bc874d2669301094eca6980
TockThomas <thomasderpolizei@gmail.com>
2021-04-22 15:27:18 +0200

    fixed connection

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import asyncio
-import datetime
-import random
 import websockets
 import car
 
 
 async def time(websocket, path):
     while True:
-        key = await websocket.recv()
-        bobby.run()
+        key = await websocket.recv().lowercase()
+        print(key)
+        #if key == "a":
+            #bobby.left()
+        #elif key == "w":
+            #bobby.run()
 
+print("Starting 4WD")
 bobby = car
-start_server = websockets.serve(time, "127.0.0.1", 5678)
+start_server = websockets.serve(time, "0.0.0.0", 5678)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
