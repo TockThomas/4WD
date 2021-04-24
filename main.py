9a5973143e7b2c38e7f13d827aab76f0499fe022
TockThomas <thomasderpolizei@gmail.com>
2021-04-24 18:34:54 +0200

    testing camera

--- main.py
+++ main.py
@@ -14,36 +14,36 @@ keys = {
 async def keyHandler(websocket, path):
     while True:
         #Steuerung
-        key = await websocket.recv()
-        print(key)
-        try:
-            if key[2] == "t":
-                keys[key[0]] = True
-            elif key[2] == "f":
-                keys[key[0]] = False
-        except:
-            print("falsche Taste")
-        if keys["w"]:
-            if keys["a"]:
-                car.left()
-            elif keys["d"]:
-                car.right()
-            else:
-                car.run()
-        elif keys["s"]:
-            if keys["a"]:
-                car.back_left()
-            elif keys["d"]:
-                car.back_right()
-            else:
-                car.back()
-        else:
-            car.brake()
+#        key = await websocket.recv()
+#        print(key)
+#        try:
+#            if key[2] == "t":
+#                keys[key[0]] = True
+#            elif key[2] == "f":
+#                keys[key[0]] = False
+#        except:
+#            print("falsche Taste")
+#        if keys["w"]:
+#            if keys["a"]:
+#                car.left()
+#            elif keys["d"]:
+#                car.right()
+#            else:
+#                car.run()
+#        elif keys["s"]:
+#            if keys["a"]:
+#                car.back_left()
+#            elif keys["d"]:
+#                car.back_right()
+#            else:
+#                car.back()
+#        else:
+#            car.brake()
         websocket.send(camera.frame())
-
-
+#
+#
 print("Starting 4WD")
-car = car.Car()
+#car = car.Car()
 camera = camera.Camera()
 start_server = websockets.serve(keyHandler, "0.0.0.0", 5678)
 
