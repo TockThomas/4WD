e8263dd85f01c977ba601253e0c27305e818b810
TockThomas <thomasderpolizei@gmail.com>
2021-04-24 17:29:37 +0200

    Added camera view in html

--- camera.py
+++ camera.py
@@ -7,12 +7,13 @@ capture = cv2.VideoCapture(0)
 
 async def keyHandler(websocket, path):
     while True:
-        ret, image = capture.read()
-        websocket.send(image)
+        ret, frame = capture.read()
+        ret, buffer = cv2.imencode(".jpg", frame)
+        await websocket.send(buffer.tobytes())
 
 
 print("Starting 4WD")
-start_server = websockets.serve(keyHandler, "0.0.0.0", 5678)
+start_server = websockets.serve(keyHandler, "localhost", 5678)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
