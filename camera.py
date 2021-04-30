a473f1ee1694db1047996034deec8f4f1e57c1a0
TockThomas <thomasderpolizei@gmail.com>
2021-04-30 15:19:20 +0200

    added multiprocessing

--- camera.py
+++ camera.py
@@ -1,11 +1,15 @@
 import cv2
+from multiprocessing import Process
 
 
 class Camera:
     def __init__(self):
         self.capture = cv2.VideoCapture(0)
+        self.frame_byte = None
+        self.process = Process(target=self.frame)
+        print("Kamera ist hochgefahren.")
 
     def frame(self):
         ret, frame = self.capture.read()
         ret, buffer = cv2.imencode(".jpg", frame)
-        return buffer.tobytes()
+        self.frame_byte = buffer.tobytes()
