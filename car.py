ec171150575cc5457295d24e927b3f43d8cadf8a
TockThomas <thomasderpolizei@gmail.com>
2021-07-28 19:23:16 +0200

    fixed Tire Class

--- car.py
+++ car.py
@@ -12,22 +12,22 @@ class Car:
         self.tire = Tire.Tire()
 
     def run(self, speed=20):
-        Tire.run(speed)
+        self.tire.run(speed)
 
     def left(self, speed=20):
-        Tire.left(speed)
+        self.tire.left(speed)
 
     def right(self, speed=20):
-        Tire.right(speed)
+        self.tire.right(speed)
 
     def back_right(self, speed=20):
-        Tire.back_right(speed)
+        self.tire.back_right(speed)
 
     def back_left(self, speed=20):
-        Tire.back_left(speed)
+        self.tire.back_left(speed)
 
     def back(self, speed=20):
-        Tire.back(speed)
+        self.tire.back(speed)
 
     def brake(self):
-        Tire.brake()
+        self.tire.brake()
