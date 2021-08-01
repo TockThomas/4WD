454309baa29b4eb49956054961b8b1f16e3933ad
TockThomas <thomasderpolizei@gmail.com>
2021-08-01 22:39:34 +0200

    improved servo reset

--- Car/Servo.py
+++ Car/Servo.py
@@ -16,23 +16,23 @@ class Servo:
         self.servo1 = GPIO.PWM(self.servoPIN1, 50)
         self.servo2 = GPIO.PWM(self.servoPIN2, 50)
         self.servo3 = GPIO.PWM(self.servoPIN3, 50)
-        self.servo_x = 6.5
-        self.servo_y = 7.5
-        self.servo_z = 6
+        self.x = 6.5
+        self.y = 7.5
+        self.z = 6
         self.start()
 
-    def servo_start(self):
-        self.servo1.start(self.servo_z)
-        self.servo2.start(self.servo_y)
-        self.servo3.start(self.servo_x)
+    def start(self):
+        self.servo1.start(self.z)
+        self.servo2.start(self.y)
+        self.servo3.start(self.x)
         self.servo_move()
 
-    def servo_move(self, pRotation="all"):
+    def move(self, pRotation="all"):
         if pRotation == "all" or pRotation == "left" or pRotation == "right":
-            self.servo1.ChangeDutyCycle(self.servo_z)
-            self.servo2.ChangeDutyCycle(self.servo_y)
+            self.servo1.ChangeDutyCycle(self.z)
+            self.servo2.ChangeDutyCycle(self.y)
         if pRotation == "all" or pRotation == "up" or pRotation == "down":
-            self.servo3.ChangeDutyCycle(self.servo_x)
+            self.servo3.ChangeDutyCycle(self.x)
         time.sleep(0.25)
         if pRotation == "all" or pRotation == "left" or pRotation == "right":
             self.servo1.ChangeDutyCycle(0)
@@ -43,36 +43,36 @@ class Servo:
     def servo(self, arg):
         print("servo")
         if arg == "up":
-            self.servo_x += 0.5
-            if self.servo_x > 11:
-                self.servo_x = 11
+            self.x += 0.5
+            if self.x > 11:
+                self.x = 11
         elif arg == "down":
-            self.servo_x -= 0.5
-            if self.servo_x < 4:
-                self.servo_x = 4
+            self.x -= 0.5
+            if self.x < 4:
+                self.x = 4
         elif arg == "left":
-            self.servo_y += 1
-            self.servo_z += 1
-            if self.servo_y > 12.5:
-                self.servo_y = 12.5
-            if self.servo_z > 11:
-                self.servo_z = 11
+            self.y += 1
+            self.z += 1
+            if self.y > 12.5:
+                self.y = 12.5
+            if self.z > 11:
+                self.z = 11
         elif arg == "right":
-            self.servo_y -= 1
-            self.servo_z -= 1
-            if self.servo_y < 3.5:
-                self.servo_y = 3.5
-            if self.servo_z < 2:
-                self.servo_z = 2
-        self.servo_move(arg)
+            self.y -= 1
+            self.z -= 1
+            if self.y < 3.5:
+                self.y = 3.5
+            if self.z < 2:
+                self.z = 2
+        self.move(arg)
 
     def reset(self):
-        self.servo_x = 6.5
-        self.servo_y = 7.5
-        self.servo_z = 6
-        self.servo1.ChangeDutyCycle(self.servo_z)
-        self.servo2.ChangeDutyCycle(self.servo_y)
-        self.servo2.ChangeDutyCycle(self.servo_x)
+        self.x = 6.5
+        self.y = 7.5
+        self.z = 6
+        self.servo1.ChangeDutyCycle(self.z)
+        self.servo2.ChangeDutyCycle(self.y)
+        self.servo2.ChangeDutyCycle(self.x)
         time.sleep(0.25)
         self.servo1.ChangeDutyCycle(0)
         self.servo2.ChangeDutyCycle(0)
