81f48728878a31e704e8f14401c017b913e9ca34
TockThomas <thomasderpolizei@gmail.com>
2021-08-02 20:09:52 +0200

    added led color

--- Car/Led.py
+++ Car/Led.py
@@ -12,15 +12,27 @@ class Led:
         GPIO.setup(self.LED_R, GPIO.OUT)
         GPIO.setup(self.LED_G, GPIO.OUT)
         GPIO.setup(self.LED_B, GPIO.OUT)
-        self.ledstatus = False
+        GPIO.output(self.LED_R, GPIO.LOW)
+        GPIO.output(self.LED_G, GPIO.LOW)
+        GPIO.output(self.LED_B, GPIO.LOW)
 
-    def changeLed(self):
-        self.ledstatus = not self.ledstatus
-        if self.ledstatus:
+        self.leds = {
+            "red": False,
+            "green": False,
+            "blue": False
+        }
+
+    def changeLed(self, pLed):
+        self.leds[pLed[0]] = pLed[1]
+        if self.leds["red"]:
             GPIO.output(self.LED_R, GPIO.HIGH)
-            GPIO.output(self.LED_G, GPIO.HIGH)
-            GPIO.output(self.LED_B, GPIO.HIGH)
         else:
             GPIO.output(self.LED_R, GPIO.LOW)
+        if self.leds["green"]:
+            GPIO.output(self.LED_G, GPIO.HIGH)
+        else:
             GPIO.output(self.LED_G, GPIO.LOW)
+        if self.leds["blue"]:
+            GPIO.output(self.LED_B, GPIO.HIGH)
+        else:
             GPIO.output(self.LED_B, GPIO.LOW)
